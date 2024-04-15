cbac3bdfe04946ae27ef4174a7da8c9eb736c60c
RyantheRanga <kryan8574@gmail.com>
2024-04-15 22:49:13 +1000

    test

--- Assets/Scripts/CameaTurnBackOn.cs
+++ Assets/Scripts/CameaTurnBackOn.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class CameaTurnBackOn : MonoBehaviour
 {
+    //test
     public CameraTurn CameraTurn;
     // Start is called before the first frame update
     void Start()
