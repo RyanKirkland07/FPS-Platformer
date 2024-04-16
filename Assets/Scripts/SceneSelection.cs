688255fdd0b4b78e3a0d421c4c971d39ae000239
RyantheRanga <kryan8574@gmail.com>
2024-04-16 19:33:51 +1000

    Fixed InAir multiplier, Add a return to menu button and scene selection

---
+++ Assets/Scripts/SceneSelection.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SceneSelection : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void SelectScene(){
+        switch(this.gameObject.name){
+            case "toLevel0":
+                SceneManager.LoadScene("level 0");
+                break;
+            case "toLevel1":
+                SceneManager.LoadScene("level 1");
+                break;
+            case "toLevel2":
+                SceneManager.LoadScene("level 2");
+                break;
+            case "toLevel3":
+                SceneManager.LoadScene("level 3");
+                break;
+            case "toLevel4":
+                SceneManager.LoadScene("level 4");
+                break;
+        }
+
+    }
+}
